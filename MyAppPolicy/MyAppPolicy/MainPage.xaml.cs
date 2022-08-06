0c258f203ea284f75c0f8a01e9e57492ce443f2d
vineeth <vineethvijayan900@gmail.com>
2022-08-06 15:18:23 +0530

    initial commit

---
+++ MyAppPolicy/MyAppPolicy/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace MyAppPolicy
+{
+    public partial class MainPage : ContentPage
+    {
+        bool m_BtnClicked;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+       
+        protected override void OnAppearing()
+        {
+            m_BtnClicked = false;
+        }
+        private void TapGestureRecognizer_Tapped(object sender,EventArgs e)
+        {
+            {
+
+                if (!m_BtnClicked)
+                {
+                    m_BtnClicked = true;
+                    Navigation.PushModalAsync(new MyPolicies());
+
+                }
+                else
+                {
+                    m_BtnClicked = false;
+                }
+            }
+        }
+
+    }
+}
---
+++ MyAppPolicy/MyAppPolicy/MyPolicies.xaml.cs
@@ -0,0 +1,16 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.CommunityToolkit;
+
+namespace MyAppPolicy
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MyPolicies : ContentPage
+    {
+        public MyPolicies()
+        {
+            InitializeComponent();
+        }
+    }
+}
